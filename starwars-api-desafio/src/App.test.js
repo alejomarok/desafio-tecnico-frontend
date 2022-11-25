c2c6c795d85506ac6d5ee3a2883651ea52548163
Alejo Marco <alejom1999@gmail.com>
2022-11-25 03:23:46 -0300

    cree un nuevo test para comprobar la api que cree para traer el arreglo de personajes, y tambien importe los hooks que voy a utilizar a continuación

--- starwars-api-desafio/src/App.test.js
+++ starwars-api-desafio/src/App.test.js
@@ -1,18 +1,35 @@
+
+import { useState, useEffect } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import data from './data.json'
 
 describe ('Stars Wars APP' , () => {
+  beforeAll(() => jest.spyOn(window, "fetch"))
   it('Should show a list of characters including Luke Skywalker', () => {
     render(<App />);
     expect(screen.getByText("Luke Skywalker")).toBeInTheDocument(); 
     
   });
 
-  it("should show a list of characters from JSON file", () => {
+  it("should show a list of characters from JSON file", async () => {
     render(<App />);
     for (let character of data.results){
       expect (screen.getByText(character.name)).toBeInTheDocument();
     }
+  });
+
+  it("should show a list of characters from the API", async () => {
+      window.fetch.mockResolvedValueOnce({
+        ok:true,
+        json: async () => data,
+      });
+      render(<App />);
+      expect(window.fetch).toHaveBeenCalledTimes(1);
+      expect(window.fetch).toHaveBeenCalledWith('https://swapi.dev/api/people/')
+
+      for (let character of data.results) {
+        expect(await screen.findByText(character.name)).toBeInTheDocument
+      }
   });
 }); 
